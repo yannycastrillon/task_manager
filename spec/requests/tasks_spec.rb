c92be505f0b29c86a6d622205cdf39d1430ab971
Yanny Castrillon <yannyandres117@gmail.com>
2025-06-02 09:26:26 -0500

    fix rubocop

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/tasks", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # Task. As you add validations to Task, be sure to
   # adjust the attributes here as well.
